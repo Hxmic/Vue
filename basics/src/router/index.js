3bdb575c7ab0e4b7e7621942f8c34149683e742a
niumx <mic7512@163.com>
2017-11-15 10:52:20 +0800

    添加index，

--- basics/src/router/index.js
+++ basics/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//import IndexPage from '../pages/index.vue'
+import IndexPage from '@/pages/index'
+import DetailPage from '@pages/detail'
 
 Vue.use(Router)
 
@@ -10,6 +13,14 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path:'/index',
+      name:'index',
+      component:IndexPage
+    },{
+      path:'/detail',
+      name:'DetailPage',
+      component:DetailPage,
     }
   ]
 })
