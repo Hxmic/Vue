20a556d268dda8db03f05a52d3f0f4e9d2ba660c
niumx <mic7512@163.com>
2017-11-17 14:10:42 +0800

    添加一个login页面。和一些css样式

---
+++ basics/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="el_login_page">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        msg:'hello login'
+      }
+    }
+  }
+</script>
--- basics/src/router/index.js
+++ basics/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/components/List'
+import Login from '@/components/Login'
+
+
 
 Vue.use(Router)
 
@@ -10,6 +13,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: List
+    },{
+      path:'/login',
+      name:'login',
+      component:Login
     }
   ]
 })
