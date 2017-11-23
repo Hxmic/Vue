e356116bcac6cd4b04acaeecd1d268a2d3e3a661
niumx <mic7512@163.com>
2017-11-23 09:45:53 +0800

    稍微大一点的项目，就如此麻烦

--- basics/src/router/index.js
+++ basics/src/router/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Not from '@/page/404'
+
+import totalpages from '@/page/activeManage/totalpages'
+import activeManage from '@/page/activeManage/index'
+import detail from '@/page/activeManage/detail'
+
+import page1 from '@/page/activeManage/page1'
+
+import step1 from '@/page/activePublic/step1'
 
 Vue.use(Router)
 
@@ -8,8 +17,30 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Not',
+      component: Not
+    },
+    {
+      path:'/activeManage',
+      component:totalpages,
+      children:[
+        {
+          path:'',
+          component:activeManage
+        },{
+          path:'detail',
+          component:detail,
+          children:[{
+            path:'',
+            component:page1,
+            children:[{
+              path:'',
+              component:step1
+            }
+            ]
+          }]
+        }
+      ]
     }
   ]
 })
