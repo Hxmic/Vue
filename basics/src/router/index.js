ddc2c317990c0ad9427ee9b9010161f2a3fbccd4
niumx <mic7512@163.com>
2017-11-24 08:55:10 +0800

    每次写vue都会报component未注册。学的还是迷糊。不太明白

--- basics/src/router/index.js
+++ basics/src/router/index.js
@@ -8,8 +8,14 @@ import activeManage from '@/page/activeManage/index'
 import detail from '@/page/activeManage/detail'
 
 import page1 from '@/page/activeManage/page1'
+import page2 from '@/page/activeManage/page2'
+import page3 from '@/page/activeManage/page3'
 
 import step1 from '@/page/activePublic/step1'
+import step2 from '@/page/activePublic/step2'
+import step3 from '@/page/activePublic/step3'
+import step4 from '@/page/activePublic/step4'
+
 
 Vue.use(Router)
 
@@ -21,24 +27,54 @@ export default new Router({
       component: Not
     },
     {
-      path:'/activeManage',
-      component:totalpages,
-      children:[
+      path: '/activeManage',
+      name: 'totalpages',
+      component: totalpages,
+      children: [
         {
-          path:'',
-          component:activeManage
-        },{
-          path:'detail',
-          component:detail,
-          children:[{
-            path:'',
-            component:page1,
-            children:[{
-              path:'',
-              component:step1
+          path: '',
+          name: '',
+          component: activeManage
+        }, {
+          path: 'detail',
+          name: 'detail',
+          component: detail,
+          children: [
+            {
+              path: '',
+              name: '',
+              component: page1,
+              children: [{
+                path: '',
+                name: '',
+                component: step1
+              }, {
+                path: 'step2',
+                name:'step2',
+                component: step2
+              },{
+                path:'step3',
+                name:'step3',
+                component:step3
+              },{
+                path:'step4',
+                name:'step4',
+                component:step4
+              }
+              ]
+            },
+            {
+              path:'page2',
+              name:'page2',
+              component:page2
+            },
+            {
+              path:'page3',
+              name:'page3',
+              component:page3
             }
+
             ]
-          }]
         }
       ]
     }
